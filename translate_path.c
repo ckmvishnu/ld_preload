1f26aa3d2ad152f9b54a846f78c20cb45aa46132
Vishnu <ckmvishnu@gmail.com>
2021-10-15 21:45:51 +0530

    Update translate_path.c

--- translate_path.c
+++ translate_path.c
@@ -14,22 +14,36 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #define FILE_NAME "/var/myfile"
-#include <dlfcn.h>
+
 
 typedef FILE* (*orig_fopen_func_type)(const char *path, const char *mode);
 typedef int (*orig_open_func_type)(const char *pathname, int flags);
 typedef int (*orig_mkdir_func_type)(const char *pathname, mode_t mode);
 typedef int (*orig_open_var_func_type)(const char *pathname, int flags, int mode);
 
+int (*old_openat)(int dirfd, const char *pathname, int flags, mode_t mode) = NULL;
+
 static int (*old_xstat)(int ver, const char *path, struct stat *buf) = NULL;
 static int (*old_xstat64)(int ver, const char *path, struct stat64 *buf) = NULL;
+static int (*old_lxstat)(int ver, const char *path, struct stat *buf) = NULL;
+static int (*old_lxstat64)(int ver, const char *path, struct stat64 *buf) = NULL;
+static int (*old_getxattr)(const char *path, const char *name, void *value, size_t size) = NULL;
+static int (*old_lgetxattr)(const char *path, const char *name, void *value, size_t size) = NULL;
+static int (*old_listxattr)(const char *path, char *list, size_t size) = NULL;
+static int (*old_llistxattr)(const char *path, char *list, size_t size) = NULL;
+//static int (*old_readlink)(const char *pathname, char *buf, size_t bufsiz) = NULL;
 static int (*old_access)(const char *pathname, int mode) = NULL;
 static int (*old_rename)(const char *pathname, const char *newpath) = NULL;
 static int (*old_unlink)(const char *path) = NULL;
 
-char system_path[][15] = { "/proc", "/dev", "/usr", "/var", "/srv" , "/sbin",
+static int (*real_stat)(const char *str, struct stat *buf);
+static int (*real_lstat)(const char *str, struct stat *buf);
+
+
+char system_path[][25] = { "/proc", "/dev", "/usr", "/var", "/srv" , "/sbin",
                            "/opt", "/run", "/root", "/selinux", "/bin", "/data",
-                           "/lib", "/etc", "/sys", "/tmp", "/home/gmadmin"};
+                           "/lib", "/etc", "/sys", "/tmp", "/home/gmadmin", "/home/gridmarkets",
+                           "/local", "/modules", "/2020"};
 
 
 int compile_regex(regex_t *regex, const char *pattern)
@@ -48,6 +62,7 @@ bool startsWith(const char *pre, const char *str)
 }
 
 
+/*
 char *recover_filename(FILE *f)
 {
     char fd_path[256];
@@ -60,6 +75,7 @@ char *recover_filename(FILE *f)
     filename[n] = '\0';
     return filename;
 }
+*/
 
 
 int skip_normalise(const char *path){
@@ -75,21 +91,66 @@ int skip_normalise(const char *path){
 void normalise_path(const char *project, const char *path, char *norm_path){
   // Var declaration
   regex_t slash_regex,
-          colon_regex;
+          colon_regex,
+          drive_char_regex,
+          drive_char_no_slash_regex;
   char drive_char;
+  int path_len;
 
+  //printf("Path [%s]\n", path);
   // Initialise
   memset(norm_path, '\0', sizeof(norm_path));
   compile_regex(&slash_regex, "^/.[A-Z,a-z,0-9]");
   compile_regex(&colon_regex, "^[A-Z,a-z]:");
+  compile_regex(&drive_char_regex, "^/[A-Z,a-z]/");
+  compile_regex(&drive_char_no_slash_regex, "^[A-Z,a-z]/");
 
+  if ((strchr(path, ':') != NULL ) && (startsWith(project, path))) {
+    //printf("Colon in path starts with project directory\n");
+    //split project path and normalise
+    path_len = strlen(path) - 1;
+    while (path_len >= 0) {
+      drive_char = path[1];
+      if (drive_char == ':') {
+        break;
+      }
+      path++;
+      path_len--;
+    }
+    //printf("Local absolute path mixed up [%s]\n", path);
+    normalise_path(project, path, norm_path);
+    for (int i=0; i < strlen(norm_path)-1; i++) {
+      if ((norm_path[i] == 92) && (norm_path[i+1] != ' ')) {
+        norm_path[i] = '/';
+      }
+    }
+    //printf("Remaped path [%s]\n", norm_path);
+  }
   // pattern match and normalise
-  if ((skip_normalise(path)) != 0){
+  else if ((skip_normalise(path)) != 0){
     if (regexec(&slash_regex, path, 0, NULL, 0) == 0){
       strcat(norm_path, project);
       // forward slash as in linux root path
       path++;
       //strcat(norm_path, project);
+      strcat(norm_path, path);
+    } else if (regexec(&drive_char_regex, path, 0, NULL, 0) == 0){
+      norm_path[0] = toupper(path[1]);
+      // forward slash as in linux root path
+      path++;
+      //strncat(norm_path, path, 1);
+      // drive char
+      path++;
+      strcat(norm_path, "__gm_colon__");
+      strcat(norm_path, path);
+    } else if (regexec(&drive_char_no_slash_regex, path, 0, NULL, 0) == 0){
+      norm_path[0] = toupper(path[0]);
+      // forward slash as in linux root path
+      //path++;
+      //strncat(norm_path, path, 1);
+      // drive char
+      path++;
+      strcat(norm_path, "__gm_colon__");
       strcat(norm_path, path);
     } else if (regexec(&colon_regex, path, 0, NULL, 0) == 0){
       strcat(norm_path, project);
@@ -106,8 +167,8 @@ void normalise_path(const char *project, const char *path, char *norm_path){
   } else {
     strcat(norm_path, path);
   }
+ //printf("Norm Path [%s]\n", norm_path);
 }
-
 
 int passthrough(const char *norm_path, int flags, va_list args){
   // If O_CREAT is used, pass on the third parameter "mode":
@@ -117,7 +178,7 @@ int passthrough(const char *norm_path, int flags, va_list args){
     orig_func = (orig_open_var_func_type)dlsym(RTLD_NEXT, "open");
     int mode = va_arg(args, int);
     va_end(args);
-    //printf("before creatig file: [%s] [%d] [%d]\n", pathname, flags, mode);
+    //printf("before creatig file: [%s] [%d]\n", norm_path, flags);
     return orig_func(norm_path, flags, 0644);
 
   } else {
@@ -140,6 +201,18 @@ int mkdir(const char *pathname, mode_t mode)
   return orig_func(norm_path, mode);
 }
 
+int open(const char *pathname, int flags, ...)
+{
+  //log_file_access(pathname);
+  char *project = getenv("TASK_DIR");
+  char norm_path[strlen(project) + strlen(pathname) + 13];
+  normalise_path(project, pathname, norm_path);
+  //printf("Normalised open path [%s]\n", norm_path);
+  va_list args;
+  va_start(args, flags);
+  return passthrough(norm_path, flags, args);
+}
+
 
 FILE* fopen(const char *pathname, const char *mode)
 {
@@ -152,6 +225,16 @@ FILE* fopen(const char *pathname, const char *mode)
   return orig_func(norm_path, mode);
 }
 
+FILE* fopen64(const char *pathname, const char *mode)
+{
+  char *project = getenv("TASK_DIR");
+  char norm_path[strlen(project) + strlen(pathname) + 13];
+  normalise_path(project, pathname, norm_path);
+  //printf("Normalised fopen64 path [%s]\n", norm_path);
+  orig_fopen_func_type orig_func;
+  orig_func = (orig_fopen_func_type)dlsym(RTLD_NEXT, "fopen64");
+  return orig_func(norm_path, mode);
+}
 
 int open64(const char *pathname, int flags, ...)
 {
@@ -164,18 +247,6 @@ int open64(const char *pathname, int flags, ...)
   return passthrough(norm_path, flags, args);
 }
 
-
-int open(const char *pathname, int flags, ...)
-{
-  //log_file_access(pathname);
-  char *project = getenv("TASK_DIR");
-  char norm_path[strlen(project) + strlen(pathname) + 13];
-  normalise_path(project, pathname, norm_path);
-  //printf("Normalised open path [%s]\n", norm_path);
-  va_list args;
-  va_start(args, flags);
-  return passthrough(norm_path, flags, args);
-}
 
 /*
 typedef ssize_t (*execve_func_t)(const char* filename, char* const argv[], char* const envp[]);
@@ -214,18 +285,59 @@ struct dirent *readdir(DIR *dir) {
   return entry;
 }
 
-int __xstat(int ver, const char *path, struct stat *buf)
+int getxattr(const char *path, const char *name, void *value, size_t size)
 {
+  //printf("getxattr [%s] [%s]\n", path, name);
   char *project = getenv("TASK_DIR");
   char norm_path[strlen(project) + strlen(path) + 13];
   normalise_path(project, path, norm_path);
-  if ( old_xstat == NULL ) {
-    old_xstat = dlsym(RTLD_NEXT, "__xstat");
+  if ( old_lgetxattr == NULL) {
+    old_lgetxattr = dlsym(RTLD_NEXT, "getxattr");
   }
+  //printf("Normakised getxattr [%s] [%s]\n", norm_path, name);
+  return old_lgetxattr(norm_path, name, value, size);
 
-  //printf("xstat %s\n", norm_path);
-  path = norm_path;
-  return old_xstat(ver, norm_path, buf);
+}
+
+int lgetxattr(const char *path, const char *name, void *value, size_t size)
+{
+  //printf("lgetxattr [%s] [%s]\n", path, name);
+  char *project = getenv("TASK_DIR");
+  char norm_path[strlen(project) + strlen(path) + 13];
+  normalise_path(project, path, norm_path);
+  if ( old_lgetxattr == NULL) {
+    old_lgetxattr = dlsym(RTLD_NEXT, "lgetxattr");
+  }
+  //printf("Normakised lgetxattr [%s] [%s]\n", norm_path, name);
+  return old_lgetxattr(norm_path, name, value, size);
+
+}
+
+int listxattr(const char *path, char *list, size_t size)
+{
+  //printf("listxattr [%s]\n", path);
+  char *project = getenv("TASK_DIR");
+  char norm_path[strlen(project) + strlen(path) + 13];
+  normalise_path(project, path, norm_path);
+  if ( old_listxattr == NULL) {
+    old_listxattr = dlsym(RTLD_NEXT, "listxattr");
+  }
+  //printf("Normalised listxattr [%s]", norm_path);
+  return old_listxattr(norm_path, list, size);
+
+}
+
+int llistxattr(const char *path, char *list, size_t size)
+{
+  //printf("llistxattr [%s]\n", path);
+  char *project = getenv("TASK_DIR");
+  char norm_path[strlen(project) + strlen(path) + 13];
+  normalise_path(project, path, norm_path);
+  if ( old_llistxattr == NULL) {
+    old_llistxattr = dlsym(RTLD_NEXT, "llistxattr");
+  }
+  //printf("Normalised llistxattr [%s]", norm_path);
+  return old_llistxattr(norm_path, list, size);
 
 }
 
@@ -238,7 +350,7 @@ int unlink(const char *path)
     old_unlink = dlsym(RTLD_NEXT, "unlink");
   }
 
-  //printf("rename %s %s\n", src_norm_path, dest_norm_path);
+  //printf("unlink %s\n", path);
   //path = norm_path;
   return old_unlink(norm_path);
 }
@@ -273,20 +385,35 @@ int access(const char *path, int mode)
   return old_access(norm_path, mode);
 }
 
-int __lxstat(int ver, const char *path, struct stat *buf)
+int __xstat(int ver, const char *path, struct stat *buf)
 {
   char *project = getenv("TASK_DIR");
   char norm_path[strlen(project) + strlen(path) + 13];
   normalise_path(project, path, norm_path);
   if ( old_xstat == NULL ) {
-    old_xstat = dlsym(RTLD_NEXT, "__lxstat");
+    old_xstat = dlsym(RTLD_NEXT, "__xstat");
   }
 
-  //printf("lxstat %s\n", norm_path);
+  //printf("xstat %s\n", norm_path);
   path = norm_path;
   return old_xstat(ver, norm_path, buf);
+
 }
 
+int __lxstat(int ver, const char *path, struct stat *buf)
+{
+  //printf("lstat [%s]\n", path);
+  char *project = getenv("TASK_DIR");
+  char norm_path[strlen(project) + strlen(path) + 13];
+  normalise_path(project, path, norm_path);
+  if ( old_lxstat == NULL ) {
+    old_lxstat = dlsym(RTLD_NEXT, "__lxstat");
+  }
+
+  //printf("lxstat normalised [%s]\n", norm_path);
+  path = norm_path;
+  return old_lxstat(ver, norm_path, buf);
+}
 
 int __xstat64(int ver, const char *path, struct stat64 *buf)
 {
@@ -307,20 +434,20 @@ int __lxstat64(int ver, const char *path, struct stat64 *buf)
   char *project = getenv("TASK_DIR");
   char norm_path[strlen(project) + strlen(path) + 13];
   normalise_path(project, path, norm_path);
-  if ( old_xstat64 == NULL ) {
-    old_xstat64 = dlsym(RTLD_NEXT, "__lxstat64");
+  if ( old_lxstat64 == NULL ) {
+    old_lxstat64 = dlsym(RTLD_NEXT, "__lxstat64");
   }
 
   //printf("lxstat64 %s\n", norm_path);
   path = norm_path;
-  return old_xstat64(ver, norm_path, buf);
+  return old_lxstat64(ver, norm_path, buf);
 }
 
 /*
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) {
     size_t (*lfwrite)(const void *, size_t, size_t, FILE*) = dlsym(RTLD_NEXT, "fwrite");
     char *fname = recover_filename(stream);
-    printf("Write to file %s\n", fname);
+    //printf("Write to file %s\n", fname);
     free(fname);
     return lfwrite(ptr, size, nmemb, stream);
 }
